071d9eb9da3dace9a86ad2ce815c3326b09adff7
Simon Gerber <simon.gerber@vshn.ch>
2021-04-15 11:16:04 +0200

    Use better file key for S3 Terraform backend
    TODO: consider switching to GitLab state backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    key                         = "tf/"
+    key                         = "cluster.tfstate"
     region                      = "us-east-1" # Ignored
     skip_credentials_validation = true
     skip_metadata_api_check     = true
