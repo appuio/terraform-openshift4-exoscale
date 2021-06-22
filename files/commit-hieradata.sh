d0faa7333fac947e164195046fb2e1c19afbdefd
Simon Gerber <simon.gerber@vshn.ch>
2021-06-22 15:16:40 +0200

    Make hieradata git commit script smarter
    Create "Update" commits in some cases instead of amending the existing
    commit.

--- files/commit-hieradata.sh
+++ files/commit-hieradata.sh
@@ -7,8 +7,10 @@ cd appuio_hieradata || exit 1
 git config user.name "${GIT_AUTHOR_NAME}"
 git config user.email "${GIT_AUTHOR_EMAIL}"
 
+amend=0
 if ! git checkout -b tf/lbaas/${cluster_id}; then 
   git checkout tf/lbaas/${cluster_id}
+  amend=1
 fi
 
 git add lbaas/${cluster_id}.yaml
@@ -17,7 +19,12 @@ status=$(git status --porcelain)
 echo "'${status}'"
 
 if [ "${status}"  == "M  lbaas/${cluster_id}.yaml" ]; then
-  git commit --amend --no-edit
-else
+  if [ "$amend" -eq 1 ]; then
+    git commit --amend --no-edit
+  else
+    git commit -m"Update LBaaS hieradata for ${cluster_id}"
+  fi
+elif [ "${status}" != "" ]; then
+  # assume new hieradata
   git commit -m "Create LBaaS hieradata for ${cluster_id}"
 fi
