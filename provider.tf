f8f0fa65a38430d07a7a5fc4ef092afa86a307b0
Simon Gerber <simon.gerber@vshn.ch>
2022-09-30 17:38:42 +0200

    Remove concluded `module_variable_optional_attrs` experiment
    This feature has been promoted to a stable feature in Terraform 1.3.0.
    For our use case we don't need to adjust anything.

--- provider.tf
+++ provider.tf
@@ -1,14 +1,5 @@
 terraform {
   required_version = ">= 1.3.0"
-  // Use experimental feature to allow making object fields optional, cf.
-  // https://www.terraform.io/docs/language/expressions/type-constraints.html#experimental-optional-object-type-attributes
-  //
-  // While there's no guarantee this feature doesn't see breaking changes even
-  // in minor releases, I think the upsides to allow users to omit some
-  // configurations for additional worker groups (e.g. node state, disk size)
-  // outweigh potential changes that we need to make in the future.
-  // -SG, 2021-07-29
-  experiments = [module_variable_optional_attrs]
   required_providers {
     exoscale = {
       source  = "exoscale/exoscale"
