9ef88c47314e75a06ec9e677ec6a5cceb39d589d
Simon Gerber <simon.gerber@vshn.ch>
2025-10-23 18:03:57 +0200

    Bump vshn-lbaas-exoscale to v6.8.0

--- lb.tf
+++ lb.tf
@@ -1,5 +1,5 @@
 module "lb" {
-  source = "git::https://github.com/appuio/terraform-modules.git//modules/vshn-lbaas-exoscale?ref=v6.6.0"
+  source = "git::https://github.com/appuio/terraform-modules.git//modules/vshn-lbaas-exoscale?ref=v6.8.0"
 
   exoscale_domain_name = exoscale_domain.cluster.name
   cluster_network = {
